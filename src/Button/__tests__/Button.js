3ddbde6e54ef7fd16cc009aa04ed0eccc11c0b8e
Todor Tsankov <todor.tsankov@marshmallow.co>
2020-02-18 13:53:24 +0000

    🧃 update Button component test suit

--- src/Button/__tests__/Button.js
+++ src/Button/__tests__/Button.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import 'jest-styled-components';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { Button } from '../Button'
 
-import {Button} from '../Button';
+test('snapshot', () => {
+  const { container } = render(<Button />)
 
-test('renders', () => {
-  const {container} = render(<Button color="green" />);
-  expect(container.firstChild).toMatchSnapshot();
-});
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+test('renders Green button', () => {
+  const { getByText } = render(
+    <Button color="green" data-testid="testBtn">
+      Push me
+    </Button>,
+  )
+  const button = getByText(/push me/i)
+
+  expect(button).toHaveTextContent('Push me')
+  expect(button).toHaveAttribute('color', 'green')
+})
+
+test('renders Disabled Blue button', () => {
+  const { container } = render(
+    <Button disabled color="blue">
+      Blue
+    </Button>,
+  )
+
+  expect(container.firstChild).toHaveAttribute('color', 'blue')
+  expect(container.firstChild).toBeDisabled()
+})
+
+test('render Block button with 100% width', () => {
+  const { container } = render(<Button block>Block</Button>)
+
+  expect(container.firstChild).toHaveStyleRule('width', '100%')
+})
+
+test('cant click Disabled button', () => {
+  const { getByText } = render(<Button disabled>Click</Button>)
+  const button = getByText(/click/i)
+
+  // debug()
+  fireEvent.click(button)
+  expect(button).toBeDisabled()
+})
